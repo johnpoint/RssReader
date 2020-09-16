704a09fdf0631a96056400f15007e3805eef8edb
johnpoint <me@lvcshu.com>
2020-09-16 09:13:19 +0800

    update :: fix

--- web/src/main.js
+++ web/src/main.js
@@ -6,7 +6,7 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "@/assets/main.less";
-import './registerServiceWorker'
+import '@/registerServiceWorker'
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
