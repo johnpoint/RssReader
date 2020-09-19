2b459b5123f8ba87ea41ea3c08bdebff7ec544f4
johnpoint <me@lvcshu.com>
2020-09-19 09:51:12 +0800

    update :: 即将到来提示

---
+++ web/src/components/Comingsoon.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="pagetip">
+    <h3>Coming soon...</h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Comingsoon"
+};
+</script>
--- web/src/views/Settings.vue
+++ web/src/views/Settings.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="home">
+    <Comingsoon />
   </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import Comingsoon from "@/components/Comingsoon.vue";
 
 export default {
-  name: "About",
+  name: "Settings",
   components: {
-  }
+    Comingsoon
+  },
 };
 </script>
