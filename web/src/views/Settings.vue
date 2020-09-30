9d140e0bbf1237fec85aaba9cc20b6c1bab388bd
johnpoint <me@lvcshu.com>
2020-09-30 13:34:30 +0800

    update :: 可选择清除本地缓存

--- web/src/views/Settings.vue
+++ web/src/views/Settings.vue
@@ -1,17 +1,55 @@
 <template>
   <div class="home">
-    <Comingsoon />
+    <b-card
+        title="本地缓存"
+        style="text-align: left"
+    >
+      <b-card-text>
+        文章: {{ localpostnum }}
+      </b-card-text>
+      <a class="card-link" @click="clearCache">清除缓存</a>
+    </b-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Comingsoon from "@/components/Comingsoon.vue";
+
+import router from "@/router";
 
 export default {
   name: "Settings",
-  components: {
-    Comingsoon
+  components: {},
+  data() {
+    return {
+      localpostnum: 0,
+      localpost: []
+    };
   },
+  beforeMount() {
+    if (window.localStorage.getItem("login") === "true") {
+      this.$store.commit("setStatus", true);
+      this.$store.commit("setjwt", window.localStorage.getItem("jwt"));
+    }
+    if (!this.$store.state.isLogin) {
+      router.push("/login");
+    }
+    this.getCache()
+  },
+  methods: {
+    getCache: function () {
+      this.localpost = []
+      Object.keys(window.localStorage).forEach(i => {
+        if (i.indexOf("post") != -1 && i.indexOf("posts") == -1) {
+          this.localpost.push(i)
+          this.localpostnum++
+        }
+      })
+    },
+    clearCache: function () {
+      this.localpost.forEach(i => {
+        window.localStorage.removeItem(i)
+      }, this.getCache())
+    }
+  }
 };
 </script>
