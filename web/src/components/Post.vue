cd598a35cf019853efe9e20a8b9ddb1016d527a2
johnpoint <me@lvcshu.com>
2020-10-13 23:22:01 +0800

    update :: 调整字体 :: 文章显示逻辑修改

--- web/src/components/Post.vue
+++ web/src/components/Post.vue
@@ -3,9 +3,9 @@
     <h1 class="title">{{ post.Title }}</h1>
     <span>{{ post.Source }}</span> |
     <a :href="post.Url">{{ $t("post.link") }}</a>
-    <b-card v-if="post.Description!=''" class="postcontext" style="margin: 15px" v-html="post.Description">
+    <b-card v-if="post.Content!==''" class="postcontext" style="margin: 15px" v-html="post.Content">
     </b-card>
-    <b-card v-else class="postcontext" style="margin: 15px" v-html="post.Content">
+    <b-card v-else class="postcontext" style="margin: 15px" v-html="post.Description">
     </b-card>
   </div>
 </template>
