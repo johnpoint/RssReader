489812e3fdcd5711a3362f6d61a7eae549e85b58
johnpoint <me@lvcshu.com>
2020-10-19 13:42:17 +0800

    update

--- src/apis/server.go
+++ src/apis/server.go
@@ -9,7 +9,7 @@ import (
 	echo2 "github.com/labstack/echo/v4"
 )
 
-const VERSION = "0.16"
+const VERSION = "0.20"
 
 func Accessible(c echo2.Context) error {
 	return c.HTML(http.StatusOK, "<h1>RssReader api</h1>(´・ω・`) 运行正常<br><hr>Ver: "+VERSION)
